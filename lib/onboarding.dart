6d044f685087b76917956382ec342ce4c729e808
Arda <ardasa716@gmail.com>
2026-08-09 13:38:31 +0200

    io

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:video_player/video_player.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
@@ -25,15 +26,25 @@ class _OnboardingState extends State<Onboarding> {
             controller: _controller,
             children: [
               Center(
-                child: Text(
-                  "1",
-                  style: Theme.of(context).textTheme.displayMedium,
+                child: Column(
+                  children: [
+                    Text("1", style: Theme.of(context).textTheme.displayMedium),
+                    Text(
+                      "This will help us customize the recipe feed to your preferences",
+                      style: Theme.of(context).textTheme.displayMedium,
+                    ),
+                  ],
                 ),
               ),
               Center(
-                child: Text(
-                  "2",
-                  style: Theme.of(context).textTheme.displayMedium,
+                child: Column(
+                  children: [
+                    Text("2", style: Theme.of(context).textTheme.displayMedium),
+                    Text(
+                      "This will help us customize the recipe feed to your preferences",
+                      style: Theme.of(context).textTheme.displayMedium,
+                    ),
+                  ],
                 ),
               ),
             ],
@@ -62,6 +73,68 @@ class _OnboardingState extends State<Onboarding> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class _OnboardingVideoPage extends StatefulWidget {
+  const _OnboardingVideoPage();
+
+  @override
+  State<_OnboardingVideoPage> createState() => _OnboardingVideoPageState();
+}
+
+class _OnboardingVideoPageState extends State<_OnboardingVideoPage> {
+  late VideoPlayerController _videoController;
+
+  @override
+  void initState() {
+    super.initState();
+    _videoController =
+        VideoPlayerController.asset('assets/videos/bgvideo25fps.mp4')
+          ..setVolume(0.0)
+          ..setLooping(true)
+          ..initialize().then((_) {
+            _videoController.play();
+            setState(() {});
+          });
+  }
+
+  @override
+  void dispose() {
+    _videoController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        // Video-Hintergrund
+        if (_videoController.value.isInitialized)
+          SizedBox.expand(
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: SizedBox(
+                width: _videoController.value.size.width,
+                height: _videoController.value.size.height,
+                child: VideoPlayer(_videoController),
+              ),
+            ),
+          )
+        else
+          Container(color: Colors.black),
+
+        // Text & Inhalt über dem Video
+        Center(
+          child: Text(
+            "Willkommen!",
+            style: Theme.of(
+              context,
+            ).textTheme.displayMedium?.copyWith(color: Colors.white),
+          ),
+        ),
+      ],
     );
   }
 }
